0b794e8993908d6b6e27219a80c5b43e252219a1
saraarka <54429786+saraarka@users.noreply.github.com>
2019-09-05 14:48:46 +0530

    g files group, suggest friend popup
    g files group, suggest friend popup

--- application/views/generstories.php
+++ application/views/generstories.php
@@ -57,15 +57,13 @@
     	    </button>
     	    <ul class="dropdown-menu list-inline dropvk">
         		<li onclick="groupsuggest(<?php echo $loadgenerstory->sid; ?>);">
-        			<a href="javascript:void(0)" data-toggle="modal" data-target="#groupsuggest" title="COMMUNITY"><i class="fa fa-users"></i></a>
+        			<a href="javascript:void(0)" title="COMMUNITY"><i class="fa fa-users"></i></a>
         		</li>
         		<li onclick="friend(<?php echo $loadgenerstory->sid;?>);">
-        			<a href="javascript:void(0)" data-toggle="modal" data-target="#friendsuggest" title="SUGGEST"><i class="fa fa-user"></i></a>
+        			<a href="javascript:void(0)" title="SUGGEST"><i class="fa fa-user"></i></a>
         		</li>
         		<li onclick="socialshare(<?php echo $loadgenerstory->sid;?>, 'story');">
-        			<a data-toggle="modal" data-target="#soc" href="javascript:void(0)" title="SOCIAL">
-        				<i class="fa fa-share-alt"></i>
-        			</a>
+        			<a data-toggle="modal" data-target="#soc" href="javascript:void(0)" title="SOCIAL"><i class="fa fa-share-alt"></i></a>
         		</li>
         	</ul>
         </div>
